9fc53c754785823b7f78f495b95b70ece186e48c
ViktorPankratov <pankratov.viktor.97@gmail.com>
2017-11-12 12:12:57 +0400

    init commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/admin/');
+define('HTTP_CATALOG', 'http://localhost/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/admin/');
+define('HTTPS_CATALOG', 'http://localhost/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/opencart/admin/');
+define('DIR_SYSTEM', 'C:/xampp/opencart/system/');
+define('DIR_IMAGE', 'C:/xampp/opencart/image/');
+define('DIR_LANGUAGE', 'C:/xampp/opencart/admin/language/');
+define('DIR_TEMPLATE', 'C:/xampp/opencart/admin/view/template/');
+define('DIR_CONFIG', 'C:/xampp/opencart/system/config/');
+define('DIR_CACHE', 'C:/xampp/opencart/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/opencart/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/opencart/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/opencart/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/opencart/system/storage/upload/');
+define('DIR_CATALOG', 'C:/xampp/opencart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'ocmanager');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/opencart/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/opencart/system/');
+define('DIR_IMAGE', 'C:/xampp/opencart/image/');
+define('DIR_LANGUAGE', 'C:/xampp/opencart/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/opencart/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/opencart/system/config/');
+define('DIR_CACHE', 'C:/xampp/opencart/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/opencart/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/opencart/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/opencart/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/opencart/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'ocmanager');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
